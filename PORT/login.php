87f0a2873d9320457e9112f1230034f8805e27c8
vincpinas <ikbengerrit92@gmail.com>
2020-10-31 22:31:41 +0100

    login expans

--- PORT/login.php
+++ PORT/login.php
@@ -4,22 +4,40 @@
 ?>
 
 <?php
-    $serverName = "localhost";
-    $dBusername = "root";
-    $dBPassword = "";
+    $host = "localhost";
+    $user = "root";
+    $password = "";
     $dBName = "portfolio";
 
-    $conn = mysqli_connect($serverName, $dBusername, $dBPassword, $dBName);
+    $con = mysqli_connect($host, $user, $password);
+    mysqli_select_db($con, $dBName) or die(mysqli_error($con));
 
-    if(!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+    if(isset($_POST['username'])){
+
+        $uname=$_POST['username'];
+        $password=$_POST['pwd'];
+
+        $sql="select * from admin where User='".$uname."'AND Password='".$password."'
+        limit 1";
+
+        $result=mysqli_query($sql);
+        
+        if(mysqli_num_rows($result)==1){
+            echo " Succesful Log in";
+            exit();
+        }
+        else{
+            echo " Invalid Password";
+            exit();
+        }
     }
+
 ?>
 
 <link rel="stylesheet" type="text/css" href="style/login.css?v=<?php echo time(); ?>">
 
 <section class="admin-login">
-    <form action="welcome.php" method="post">
+    <form action="admin.php" method="post">
         <h2>Login</h2>
             <input type="text" class="form-control input" name="username" placeholder="Name..">
             <input type="password" class="form-control input" name="pwd" placeholder="Password..">
