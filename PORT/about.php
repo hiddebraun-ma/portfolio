8e99f64bc7f9e203fd46c4b342bf3e581e61a37a
vincpinas <ikbengerrit92@gmail.com>
2020-11-03 21:20:54 +0100

    .

--- PORT/about.php
+++ PORT/about.php
@@ -12,13 +12,11 @@
             <h2>Who Am I?</h2>
                 <p class="who-am-text">My name is Vincent Pinas.<br> I'm based in the Netherlands where I was born.<br> I got into coding at the age of 16 when I was searching for a new school.</p>
             <h2>What matters to me.</h2>
-                <p class="what-important-text">Usability, flexibility and user experience are what makes a great app,<br> that's why it is important to me that all these things are up to standards when working with my clients.</p>
+                <p class="what-important-text">Usability, flexibility and user experience are what makes a great app.<br>
+                 That's why it is important to me that all these things are up to standards when working with my clients.<br
+                >A nice working environment for both me and the client are top priority.</p>
             <h2>What I do.</h2>
                 <p class="what-do-text">I primarily makes content on the web like apps and websites using react and firebase.</p>
-            <div class="skills">
-                <span>Web Design</span>
-                <span>Photoshop</span>
-                <span>Coding</span>
             </div>
         </div>
     </div>
--- PORT/footer.php
+++ PORT/footer.php
@@ -2,10 +2,4 @@
 <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css?v=<?php echo time(); ?>">
 
 
-<footer>
-    <ul class="footer__links">
-        <li class="active"><a href="">Socials</a></li>
-        <li class="active"><a href="">Explore</a></li>
-        <li class="active"><a href="">Info</a></li>
-    </ul>
-</footer>
+<footer><h5>Copyright &#169 2020 <a href="index.php" id="vincent">Vincent</a>. All Rights Reserved</h5></footer>
