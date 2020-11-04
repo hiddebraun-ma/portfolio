1198d734772b44897c4ab7857ea94ccbf944c0f4
vincpinas <ikbengerrit92@gmail.com>
2020-11-04 22:07:01 +0100

    about page half finish

--- PORT/about.php
+++ PORT/about.php
@@ -6,22 +6,29 @@
 <link rel="stylesheet" type="text/css" href="style/about.css?v=<?php echo time(); ?>">
 
 <body>
-    <div class="about-section">
-        <div class="inner-container">
-            <h1>About Me</h1>
-            <h2>Who Am I?</h2>
-                <p class="who-am-text">My name is Vincent Pinas.<br> I'm based in the Netherlands where I was born.<br> I got into coding at the age of 16 when I was searching for a new school.</p>
-            <h2>What matters to me.</h2>
-                <p class="what-important-text">Usability, flexibility and user experience are what makes a great app.<br>
-                 That's why it is important to me that all these things are up to standards when working with my clients.<br
-                >A nice working environment for both me and the client are top priority.</p>
-            <h2>What I do.</h2>
-                <p class="what-do-text">I primarily makes content on the web like apps and websites using react and firebase.</p>
+<video id=video-comp1 poster=poster.JPG autoplay loop muted>
+    <source src="background/2123.mp4" type="video/mp4">
+</video><!-- Notebook.html background video.-->  
+    <div class="container">
+        <div class="topbar">
+            <div class="circles">
+                <div class="circle1"></div>
+                <div class="circle2"></div>
+                <div class="circle3"></div>
+            </div>
+        </div>
+        <div class="screen">
+            <div class="overflow">
+                <h1>About Me</h1>
+                <h2>Who Am I?</h2>
+                    <p class="who-am-text">My name is Vincent Pinas.<br> I'm based in the Netherlands where I was born.<br> I got into coding at the age of 16 when I was searching for a new school.</p>
+                <h2>What matters to me.</h2>
+                    <p class="what-important-text">Usability, flexibility and user experience are what makes a great app.<br>
+                    That's why it is important to me that all these things are up to standards when working with my clients.<br
+                    >A nice working environment for both me and the client are top priority.</p>
+                <h2>What I do.</h2>
+                    <p class="what-do-text">I primarily make content for the web, things I work on include,<br> mobile apps, websites and front end design.</p>
             </div>
         </div>
     </div>
 </body>
-
-<?php
-    include_once 'footer.php'
-?>
