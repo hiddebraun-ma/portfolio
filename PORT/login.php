4775157fbab3333531cae3a24ba612d15686047f
Hidde Braun <hidde.braun@gmail.com>
2020-11-09 09:37:19 +0100

    Hulp bij inloggen

--- PORT/login.php
+++ PORT/login.php
@@ -1,5 +1,5 @@
 <?php
-    include_once "header.php"
+include_once "header.php"
 
 ?>
 
@@ -11,58 +11,66 @@
 $errors = [];
 
 if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+
+	$username = $_POST['username'];
+	$userpass = $_POST['password'];
 
 
-$host = "localhost";
-$root = "root";
-$password = "";
-$dBName = "portfolio";
+	$host     = "localhost";
+	$root     = "root";
+	$password = "";
+	$dBName   = "vincent";
 
-$conn = mysqli_connect($host, $root, $password);
+	$dsn = 'mysql:dbname=' . $dBName . ';host=' . $host . ';';
+	try {
+		$conn = new PDO( $dsn, $root, $password );
+	} catch ( PDOException $e ) {
+		echo 'Connection failed: ' . $e->getMessage();
+		exit;
+	}
 
-$sql = 'SELECT * FROM `admin` WHERE `User` = :username';
-$statement = $conn->prepare( $sql );
+	$sql       = 'SELECT * FROM `admin` WHERE `User` = :username';
+	$statement = $conn->prepare( $sql );
 
-$params = [
-    'username' => $username
-];
+	$params = [
+		'username' => $username
+	];
 
-$result = $statement->execute( $params );
+	$result = $statement->execute( $params );
 
-if ( $statement->rowCount() === 1 ) {
-    $user = $statement->fetch();
-} else {
-    $errors['email'] = 'Onbekend account';
-}
+	if ( $statement->rowCount() === 1 ) {
+		$user = $statement->fetch();
 
-if ( password_verify($password, $user['wachtwoord'])) {
-    session_start();
-    $_SESSION['user_id']  = $user['id'];
-    $_SESSION['voornaam'] = $user['voornaam'];
+		if ( password_verify( $userpass, $user['wachtwoord'] ) ) {
+			session_start();
+			$_SESSION['user_id']  = $user['id'];
+			$_SESSION['voornaam'] = $user['voornaam'];
 
-    header( 'Location: admin.php');
-    exit();
-}
+			header( 'Location: admin.php' );
+			exit();
+		} else {
+			$errors['password'] = 'Invalid Password';
+		}
 
-} else {
-    $errors['password'] = 'Invalid Password';
+	} else {
+		$errors['email'] = 'Onbekend account';
+	}
+
 }
 ?>
 
 <section class="admin-login">
     <form method="POST" action="login.php">
         <h2>Login</h2>
-            <input type="text" class="form-control input" name="username" placeholder="Name..">
-            <?php if (isset( $errors['username'] ) ): ?>
-                <span class="error"><?php echo $errors['username'] ?></span>
-            <?php endif; ?>
+        <input type="text" class="form-control input" name="username" placeholder="Name..">
+		<?php if ( isset( $errors['username'] ) ): ?>
+            <span class="error"><?php echo $errors['username'] ?></span>
+		<?php endif; ?>
 
-            <input type="password" class="form-control input" name="password" placeholder="Password..">
-            <?php if (isset( $errors['password'] ) ): ?>
-                <span class="error"><?php echo $errors['password'] ?></span>
-            <?php endif; ?>
-            <button type="submit" class="form-control submit" name="submit">Login</button>
+        <input type="password" class="form-control input" name="password" placeholder="Password..">
+		<?php if ( isset( $errors['password'] ) ): ?>
+            <span class="error"><?php echo $errors['password'] ?></span>
+		<?php endif; ?>
+        <button type="submit" class="form-control submit" name="submit">Login</button>
     </form>
 </section>
